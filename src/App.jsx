228224f429fa3481eb9e50ddd0e83c01e415cac7
Evgeny Orekhov <eorekhov@axcient.com>
2020-07-21 00:52:14 +0300

    Refactor components

--- src/App.jsx
+++ src/App.jsx
@@ -15,121 +15,64 @@ function isSelectingText() {
   return window.getSelection().type === "Range";
 }
 
-function Task({ task, state, actions }) {
-  const { value, content, isDone, children, path, id } = task;
+function confirmRemoval(children, onConfirm, onCancel) {
+  if (
+    window.confirm(
+      `Remove this task and and its ${children.length} subtask${
+        children.length > 1 ? "s" : ""
+      }?`
+    )
+  ) {
+    onConfirm();
 
-  const taskReference = useRef();
-  const [shouldScrollIntoView, setShouldScrollIntoView] = useState(false);
+    return;
+  }
 
-  useEffect(() => {
-    if (shouldScrollIntoView) {
-      taskReference.current.scrollIntoView(false);
-      setShouldScrollIntoView(false);
-    }
-  }, [shouldScrollIntoView]);
+  if (onCancel) {
+    onCancel();
+  }
+}
 
-  function handleTaskKeyDown(event) {
-    if (
-      state.editingValuePath.length !== 0 ||
-      state.editingContentPath.length !== 0
-    ) {
+function handleDelete(children, callback) {
+  if (children.length !== 0) {
+    confirmRemoval(children, callback);
+
+    return;
+  }
+
+  callback();
+}
+
+function Value({ task, state, actions }) {
+  const { value, children, path } = task;
+
+  function handleEnterKey(event) {
+    if (event.target.value.trim() === "" && children.length !== 0) {
+      confirmRemoval(
+        children,
+        () => actions.setValue(""),
+        actions.editingValuePath.reset
+      );
+
       return;
     }
 
-    if (event.key === " ") {
-      actions.tasks.toggle(path);
+    actions.setValue(event.target.value);
 
-      event.preventDefault();
-
-      return;
-    }
-
-    if (event.key === "Enter") {
+    if (event.target.value.trim() !== "") {
       if (event.ctrlKey) {
         actions.addNextTask(path);
-
-        return;
       }
 
       if (event.shiftKey) {
         actions.addSubtask(path);
-
-        return;
-      }
-
-      actions.editingValuePath.set(path);
-
-      return;
-    }
-
-    if (event.key === "Delete") {
-      if (children.length !== 0) {
-        if (
-          window.confirm(
-            `Remove this task and and its ${children.length} subtask${
-              children.length > 1 ? "s" : ""
-            }?`
-          )
-        ) {
-          actions.tasks.delete(path);
-
-          return;
-        }
-
-        return;
-      }
-
-      actions.tasks.delete(path);
-    }
-
-    if (event.shiftKey) {
-      if (event.key === "ArrowUp") {
-        actions.tasks.moveUp(path);
-
-        setShouldScrollIntoView(true);
-
-        return;
-      }
-
-      if (event.key === "ArrowDown") {
-        actions.tasks.moveDown(path);
-
-        setShouldScrollIntoView(true);
       }
     }
   }
 
-  function handleValueKeyDown(event) {
+  function handleKeyDown(event) {
     if (event.key === "Enter") {
-      if (event.target.value.trim() === "" && children.length !== 0) {
-        if (
-          window.confirm(
-            `Remove this task and and its ${children.length} subtask${
-              children.length > 1 ? "s" : ""
-            }?`
-          )
-        ) {
-          actions.setValue("");
-
-          return;
-        }
-
-        actions.editingValuePath.reset();
-
-        return;
-      }
-
-      actions.setValue(event.target.value);
-
-      if (event.target.value.trim() !== "") {
-        if (event.ctrlKey) {
-          actions.addNextTask(path);
-        }
-
-        if (event.shiftKey) {
-          actions.addSubtask(path);
-        }
-      }
+      handleEnterKey(event);
 
       return;
     }
@@ -146,24 +89,42 @@ function Task({ task, state, actions }) {
   }
 
   function handleDeleteClick() {
-    if (children.length !== 0) {
-      if (
-        window.confirm(
-          `Remove this task and and its ${children.length} subtask${
-            children.length > 1 ? "s" : ""
-          }?`
-        )
-      ) {
-        actions.tasks.delete(path);
-
-        return;
-      }
-
-      return;
-    }
-
-    actions.tasks.delete(path);
+    handleDelete(children, () => actions.tasks.delete(path));
   }
+
+  return (
+    <div className="value">
+      {equals(path, state.editingValuePath) ? (
+        <input
+          defaultValue={value}
+          onBlur={(event) => actions.setValue(event.target.value)}
+          onKeyDown={handleKeyDown}
+          autoFocus
+        />
+      ) : (
+        <div
+          className="clickable-value"
+          onClick={() =>
+            !isSelectingText() && actions.editingValuePath.set(path)
+          }
+        >
+          <ReactMarkdown source={value} escapeHtml={false} />
+        </div>
+      )}
+      <button
+        type="button"
+        className="delete"
+        tabIndex="-1"
+        onClick={handleDeleteClick}
+      >
+        ✕
+      </button>
+    </div>
+  );
+}
+
+function Content({ task, state, actions }) {
+  const { content, path } = task;
 
   function handleContentClick() {
     if (window.getSelection().type === "Range") {
@@ -196,12 +157,150 @@ function Task({ task, state, actions }) {
   }
 
   return (
+    <div
+      tabIndex={
+        equals(path, state.editingContentPath) ||
+        (!content && !equals(path, state.editingValuePath))
+          ? "-1"
+          : "0"
+      }
+      onClick={handleContentClick}
+      onFocus={handleContentFocus}
+      onKeyDown={handleContentKeyDown}
+    >
+      {equals(path, state.editingContentPath) ? (
+        <AceEditor
+          mode="markdown"
+          theme="tomorrow_night_bright"
+          className="editable-content"
+          width="100%"
+          height="100%"
+          defaultValue={content}
+          focus
+          onFocus={(event, editor) => editor.navigateFileStart()}
+          commands={[
+            {
+              name: "save",
+              bindKey: { win: "Ctrl-Enter", mac: "Command-Enter" },
+
+              exec(editor) {
+                actions.setContent(editor.getValue());
+              },
+            },
+            {
+              name: "cancel",
+              bindKey: { win: "Escape", mac: "Escape" },
+
+              exec() {
+                actions.editingContentPath.reset();
+              },
+            },
+          ]}
+          onBlur={(event, editor) => actions.setContent(editor.getValue())}
+        />
+      ) : (
+        <div className="content">
+          <ReactMarkdown source={content} escapeHtml={false} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Task({ task, state, actions }) {
+  const { isDone, children, path, id } = task;
+
+  const taskReference = useRef();
+  const [shouldScrollIntoView, setShouldScrollIntoView] = useState(false);
+
+  useEffect(() => {
+    if (shouldScrollIntoView) {
+      taskReference.current.scrollIntoView(false);
+      setShouldScrollIntoView(false);
+    }
+  }, [shouldScrollIntoView]);
+
+  function handleSpaceKey(event) {
+    actions.tasks.toggle(path);
+
+    event.preventDefault();
+  }
+
+  function handleEnterKey(event) {
+    if (event.ctrlKey) {
+      actions.addNextTask(path);
+
+      return;
+    }
+
+    if (event.shiftKey) {
+      actions.addSubtask(path);
+
+      return;
+    }
+
+    actions.editingValuePath.set(path);
+  }
+
+  function handleDeleteKey() {
+    handleDelete(children, () => actions.tasks.delete(path));
+  }
+
+  function handleArrowKeys(event) {
+    if (event.shiftKey) {
+      if (event.key === "ArrowUp") {
+        actions.tasks.moveUp(path);
+
+        setShouldScrollIntoView(true);
+
+        return;
+      }
+
+      if (event.key === "ArrowDown") {
+        actions.tasks.moveDown(path);
+
+        setShouldScrollIntoView(true);
+      }
+    }
+  }
+
+  // eslint-disable-next-line max-statements
+  function handleKeyDown(event) {
+    if (
+      state.editingValuePath.length !== 0 ||
+      state.editingContentPath.length !== 0
+    ) {
+      return;
+    }
+
+    if (event.key === " ") {
+      handleSpaceKey(event);
+
+      return;
+    }
+
+    if (event.key === "Enter") {
+      handleEnterKey(event);
+
+      return;
+    }
+
+    if (event.key === "Delete") {
+      handleDeleteKey();
+
+      return;
+    }
+
+    handleArrowKeys(event);
+  }
+
+  return (
     <li key={id} className={`${isDone ? "is-done" : ""}`}>
       <div
         ref={taskReference}
         className="task"
         tabIndex="0"
-        onKeyDown={handleTaskKeyDown}
+        onKeyDown={handleKeyDown}
       >
         <input
           type="checkbox"
@@ -209,33 +308,7 @@ function Task({ task, state, actions }) {
           tabIndex="-1"
           onChange={() => actions.tasks.toggle(path)}
         />
-        <div className="value">
-          {equals(path, state.editingValuePath) ? (
-            <input
-              defaultValue={value}
-              onBlur={(event) => actions.setValue(event.target.value)}
-              onKeyDown={handleValueKeyDown}
-              autoFocus
-            />
-          ) : (
-            <div
-              className="clickable-value"
-              onClick={() =>
-                !isSelectingText() && actions.editingValuePath.set(path)
-              }
-            >
-              <ReactMarkdown source={value} escapeHtml={false} />
-            </div>
-          )}
-          <button
-            type="button"
-            className="delete"
-            tabIndex="-1"
-            onClick={handleDeleteClick}
-          >
-            ✕
-          </button>
-        </div>
+        <Value task={task} state={state} actions={actions} />
       </div>
       <ul className="tasks">
         {children.map((subtask) => (
@@ -247,53 +320,7 @@ function Task({ task, state, actions }) {
           />
         ))}
       </ul>
-      <div
-        tabIndex={
-          equals(path, state.editingContentPath) ||
-          (!content && !equals(path, state.editingValuePath))
-            ? "-1"
-            : "0"
-        }
-        onClick={handleContentClick}
-        onFocus={handleContentFocus}
-        onKeyDown={handleContentKeyDown}
-      >
-        {equals(path, state.editingContentPath) ? (
-          <AceEditor
-            mode="markdown"
-            theme="tomorrow_night_bright"
-            className="editable-content"
-            width="100%"
-            height="100%"
-            defaultValue={content}
-            focus
-            onFocus={(event, editor) => editor.navigateFileStart()}
-            commands={[
-              {
-                name: "save",
-                bindKey: { win: "Ctrl-Enter", mac: "Command-Enter" },
-
-                exec(editor) {
-                  actions.setContent(editor.getValue());
-                },
-              },
-              {
-                name: "cancel",
-                bindKey: { win: "Escape", mac: "Escape" },
-
-                exec() {
-                  actions.editingContentPath.reset();
-                },
-              },
-            ]}
-            onBlur={(event, editor) => actions.setContent(editor.getValue())}
-          />
-        ) : (
-          <div className="content">
-            <ReactMarkdown source={content} escapeHtml={false} />
-          </div>
-        )}
-      </div>
+      <Content task={task} state={state} actions={actions} />
     </li>
   );
 }
